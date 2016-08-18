b319cf168750af976751685b05558148d21c032d
Brendon Irwin <bren1818@gmail.com>
2016-08-18 18:44:38 -0400

    Started roughing in the scene editor and creator

--- scenescreatedit.php
+++ scenescreatedit.php
@@ -1,29 +1,14 @@
 <?php
 	/*
 	 *
-	 * TCP Ligthing Web UI Scenes Script - By Brendon Irwin
+	 * TCP Ligthing Web UI Scenes Create/Edit Script - By Brendon Irwin
 	 * 
 	 */
 
 	include "include.php";
 
 	if (strtoupper($_SERVER['REQUEST_METHOD']) == 'POST'){
-		//run scene
-		if( isset($_POST['scene']) && $_POST['scene'] != "" ){
-			$sceneID = $_POST['scene'];
-			$action = $_POST['action'];
-			if( $action == "on"){
-				$CMD = "cmd=SceneRun&data=<gip><version>1</version><token>".TOKEN."</token><sid>".$sceneID."</sid><val>1</val></gip>"; //val 1 is on 0 is off
-			}else if( $action == "off"){
-				$CMD = "cmd=SceneRun&data=<gip><version>1</version><token>".TOKEN."</token><sid>".$sceneID."</sid><val>0</val></gip>"; //val 1 is on 0 is off
-			}else if($action == "delete"){
-				$CMD = "cmd=SceneDelete&data=<gip><version>1</version><token>".TOKEN."</token><sid>".$sceneID."</sid></gip>"; 
-			}
-			
-			$result = getCurlReturn($CMD);
-			$array = xmlToArray($result);
-			echo json_encode( array("scene" => $sceneID, "return" => $array) );
-		}
+		
 		exit;
 	}
 ?>
@@ -49,7 +34,7 @@
 	<script src="scripts.js"></script>
 </head>
 <body>
-<div id="toolBar"><a href="index.php">Lighting Controls</a> | <a href="scheduler.php">Lighting Scheduler</a> | <a href="apitest.php">API Test Zone</a></div>
+<div id="toolBar"><a href="index.php">Lighting Controls</a> | <a href="scheduler.php">Lighting Scheduler</a> | <a href="scenes.php">Scenes/Smart Control</a> | <a href="apitest.php">API Test Zone</a></div>
 <script>
 	$(function(){
 		$('.activateScene').click(function(event){
@@ -77,17 +62,10 @@
 				$.post( "scenes.php", { scene: sceneID, action: 'delete' })
 				  .done(function( data ) {
 					console.log( "Response " + data );
-					location.reload();
+					window.location = "scenes.php";
 				});
 			}
 		});
-		
-		$('.editScene').click(function(event){
-			event.preventDefault();
-			var sceneID = $(this).data("scene");
-			window.location = "scenescreatedit.php?SID=" + sceneID;
-		});
-		
 	});
 </script>	
 <?php
@@ -98,8 +76,10 @@
 	$array = xmlToArray($result);
 	
 	$scenes = $array["scene"];
-	if( is_array($scenes) ){
+	if( is_array($scenes) && isset($_REQUEST['SID']) && $_REQUEST['SID'] != ""){
+		$scene =  $_REQUEST['SID'];
 		for($x = 0; $x < sizeof($scenes); $x++){
+			if($scenes[$x]["sid"] == $scene ){
 			?>
 			<div class="" id="scene-id-<?php echo $scenes[$x]["sid"]; ?>">
 				<h3><?php echo $scenes[$x]["name"]; ?></h3>
@@ -114,17 +94,22 @@
 				</p>
 				<button data-scene="<?php echo $scenes[$x]["sid"]; ?>" class="activateScene">Activate</button>
 				<button data-scene="<?php echo $scenes[$x]["sid"]; ?>" class="deactivateScene">De-Activate</button>
-				<button data-scene="<?php echo $scenes[$x]["sid"]; ?>" class="editScene">Edit</button> 
+				<!-- <button data-scene="<?php echo $scenes[$x]["sid"]; ?>" class="editScene">Edit</button> -->
 				<button data-scene="<?php echo $scenes[$x]["sid"]; ?>" class="deleteScene">Delete</button>
 			</div>
 			<?php
+			pa($scenes[$x]);
+			
+			}
 		}
 		
+	}else{
+		?>
+		<h2>Create Scene</h2>
+		<?php
 	}
 	
-	//pa($array);
+	
 ?>
-<br />
-<a href="scenescreatedit.php">Create Scene</a>
 </body>
 </html>
