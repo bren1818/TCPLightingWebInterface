e5fee0e464e65a9d2619181eb4d290197c4d2fa8
Brendon Irwin <bren1818@gmail.com>
2016-08-25 20:59:07 -0400

    Some mobile friendly additions

--- index.php
+++ index.php
@@ -8,6 +8,7 @@
 	<link rel="manifest" href="favicons/manifest.json">
 	<link rel="mask-icon" href="favicons/safari-pinned-tab.svg" color="#5bbad5">
 	<link rel="shortcut icon" href="favicons/favicon.ico">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="apple-mobile-web-app-title" content="TCP Lighting">
 	<meta name="application-name" content="TCP Lighting">
 	<meta name="msapplication-config" content="favicons/browserconfig.xml">
