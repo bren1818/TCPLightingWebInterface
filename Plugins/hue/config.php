d11069071667764a94180f198cf796163495a1dd
brendon irwin <bren1818@gmail.com>
2016-12-18 22:35:09 -0500

    hue lights working.. with one colour/hue

--- Plugins/hue/config.php
+++ Plugins/hue/config.php
@@ -59,15 +59,15 @@
 						$hueDevice->setName( $device['name'] );
 						$hueDevice->setState( $device['state']['on'] );
 						$hueDevice->setBrightness( intval(( $device['state']['bri'] / 255 ) * 100) );
-						
-						
-						$this->addDevice( $hueDevice );
-						
-						$this->runCommand('/api/'.$this->getToken().'/lights/'.$x.'/state', '{"on":false}', "PUT");
-						$x++;
 						//hue
 						//sat
-						//effect
+						
+						$hueDevice->setDeviceType( $device['type'] );
+						$hueDevice->setDeviceOwner( $this->getID() );
+						$this->addDevice( $hueDevice );
+			
+						$x++;
+						
 						
 						/*
 							
@@ -107,7 +107,7 @@
 					
 					
 					}					
-					echo '<pre>'.print_r(  $this  , true).'</pre>';
+					//echo '<pre>'.print_r(  $this  , true).'</pre>';
 				}
 			}
 			
@@ -125,7 +125,6 @@
 			if( $METHOD == "PUT" ){
 				curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "PUT");
 				curl_setopt($ch, CURLOPT_POSTFIELDS, $CMD);
-				echo $URL.$PATH.'<br />';
 			}
 			
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -136,16 +135,65 @@
 			return $data;
 		}
 		
+		
+		
+		
+		
+		
+		function turnDeviceOn( $ID ){ /*Override*/ 
+			$this->runCommand('/api/'.$this->getToken().'/lights/'.$ID.'/state', '{"on":true}', "PUT");
+		}
+		
+		function turnDeviceOff( $ID ){ /*Override*/ 
+			$this->runCommand('/api/'.$this->getToken().'/lights/'.$ID.'/state', '{"on":false}', "PUT");
+		}
+		
+		function dimDevice( $ID, $opacity){ /*Override*/ 
+			 $this->runCommand('/api/'.$this->getToken().'/lights/'.$ID.'/state', '{"on":true, "sat":254, "bri":'.intval( ($opacity / 100) * 255 ).',"hue":10000}', "PUT");
+		}
+		
+		
+		
+		
+		function turnCollectionOn( $ID ){ /*Override*/}
+		function turnCollectionOff( $ID ){ /*Override*/ }
+		function dimCollection( $ID, $opacity){ /*Override*/ }
+		
+		function turnAllOn(){ /*Override*/ 
+			for($x=1; $x<= sizeof($this->getDevices()); $x++){
+				$this->runCommand('/api/'.$this->getToken().'/lights/'.$x.'/state', '{"on":true}', "PUT");
+			}
+		}
+		
+		function turnAllOff(){ /*Override*/ 
+			for($x=1; $x<= sizeof($this->getDevices()); $x++){
+				$this->runCommand('/api/'.$this->getToken().'/lights/'.$x.'/state', '{"on":false}', "PUT");
+			}
+		}
+		
+		
+		function dimAll($opacity){ /*Override*/ }
+		
+		
 	}
 	
 	class hue_device extends base_device{
 		public $hueID;
+		public $deviceype;
 		
 		function setHueID($id){
 			$this->hueID = $id;
 		}
 		function getHueID(){
 			return $this->hueID;
+		}
+		
+		function getDeviceType(){
+			return $this->deviceType;
+		}
+		
+		function setDeviceType($type){
+			$this->deviceType = $type;
 		}
 		
 		function __construct(){
@@ -160,6 +208,29 @@
 			//$this->setDeviceOwner( "" );
 			
 		}
+	
+	
+		function renderDevice(){
+			
+			echo '<div data-bridgeID="'.$this->getDeviceOwner().'" class="'.( $this->getState() == 0 ? 'unplugged' : 'plugged' ).' device '.( $this->getState() == 1 ? 'light-on' : 'light-off' ).' '.( $this->getDeviceType() == 'Light Fixture' ? 'light-fixture' : '' ).'" data-device-id="'.$this->getID().'">'; //power > 0 then enabled 
+				//level = brightness
+				//state = on or off
+				echo '<p>'.$this->getName().'</p>';
+				
+				echo '<button data-device-id="'.$this->getID().'" class="onOffDeviceToggleButton buttonOn">On</button> | <button data-device-id="'.$this->getID().'" class="onOffDeviceToggleButton buttonOff">Off</button>';
+				echo '<div class="clear"></div>';
+				echo '<p>Brightness:</p>';
+				echo '<div class="device-slider" data-value="'.( ( $this->getBrightness() ) ? $this->getBrightness() : 100).'" data-device-id="'. $this->getID().'"></div>';
+			echo '</div>';
+			
+		}
+	
+	
+	
+	
+	
+	
+	
 	
 	}
 	
