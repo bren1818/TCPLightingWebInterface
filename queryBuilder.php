e687ce201cdd4246bb1f5dfa9613a7ab5c745a86
Brendon Irwin <bren1818@gmail.com>
2017-12-17 20:43:39 -0500

    Added option to pre-face Lights with TCP or something of the like. Made verbiage more consistent. Fixed bug with Scene Run command.

--- queryBuilder.php
+++ queryBuilder.php
@@ -4,12 +4,16 @@ pageHeader("IFTTT Query Builder");
 ?>
 <style>
 p{margin: 10px 0; }
+.response{ cursor: pointer; }
+.response:hover{ color: #f00; }
 </style>
 <div class="container" style="padding: 20px; background-color: #fff; border: 1px solid #000;">
 <p>Use this to assist with creation of the IFTTT web-hooks quickly</p>
 <p>I suggest setting up a Google Assistant Hook which then calls a webhook with the API string. Follow the <a target="_blank" href="https://github.com/bren1818/TCPLightingWebInterface/wiki/IFTTT-Integration">excellent guide</a> written by <a target="_blank" href="https://github.com/sktaylortrash">@sktaylortrash</a> in the wiki and feel free to ask a question in the forum</p>
 
 <p>It is recommended that use SSL if you're able. This will work on non-standard ports, (ports other than 80/443) but you will need to setup port-forwarding and setup your webserver to listen on those ports. <a target="_blank" href="https://github.com/sktaylortrash">@sktaylortrash</a> has <a target="_blank" href="https://github.com/bren1818/TCPLightingWebInterface/wiki/Installation">documented</a> how to set everything up on both a <a target="_blank" href="https://github.com/bren1818/TCPLightingWebInterface/wiki/Installation-for-Apache-on-Raspbian">Raspberry Pi / Linux Environment</a>, or using <a target="_blanks" href="https://github.com/bren1818/TCPLightingWebInterface/wiki/Installation-for-Apache-on-XAMPP-Windows">XAMPP on Windows</a>.</p> 
+
+
 
 <?php 
 if( ALLOW_EXTERNAL_API_ACCESS == 1 ){
@@ -30,6 +34,9 @@ $(function(){
 		var id = $(this).find('option:selected').attr('data-device-id');
 		var type = $(this).find('option:selected').attr('data-device-type');
 		var roomName = "";
+		var preface = $('#preface').val();
+		if( preface.length > 0 ){ preface = ' ' + preface + ' '; }
+		
 		$('#appletImage').html('');
 		
 		if( type == "room" || type == "light"){
@@ -41,67 +48,69 @@ $(function(){
 			$('#appletImage').html('<img src="css/images/IFTTTAppletPhrase.png"/>');
 			
 			if( type == "room"  ){
-				$('#command_on_1').html("TCP " +  roomName + " lights ON");
-				$('#command_on_2').html(roomName + " TCP lights ON");
-				$('#command_on_3').html(roomName + " lights on");
-				$('#command_on_response').html("Turning " + roomName + " lights ON");
+				$('#command_on_1').html(roomName + preface + " lights ON");
+				$('#command_on_2').html("Switch " + roomName + preface + " lights ON");
+				$('#command_on_3').html("Turn " + roomName + preface + " lights on");
+				$('#command_on_response').html("Turning " + roomName + preface + " lights ON");
 				$('#command_on_url').html( externalAddr + ( (externalPort != 80 || externalPort != 443) ?  ':' + externalPort : '' ) + '/api.php?fx=toggle&type=room&uid=' + id + '&val=1' + ( requireExtPass == 1 ? '&password=' + externalPass : '')  );
 				
-				$('#command_off_1').html("TCP " +  roomName + " lights off");
-				$('#command_off_2').html(roomName + " TCP Lights off");
-				$('#command_off_3').html(roomName + " lights off");
+				$('#command_off_1').html(roomName + preface + " lights off");
+				$('#command_off_2').html("Switch " + roomName + preface +" Lights off");
+				$('#command_off_3').html("Turn " + roomName + preface + " lights off");
 				$('#command_off_response').html("Turning off " + roomName + " lights");
 				$('#command_off_url').html( externalAddr + ( (externalPort != 80 || externalPort != 443) ?  ':' + externalPort : '' ) + '/api.php?fx=toggle&type=room&uid=' + id + '&val=0' + ( requireExtPass == 1 ? '&password=' + externalPass : '') );
 				
-				$('#command_dim_1').html("TCP " +  roomName + " lights to # % brightness");
-				$('#command_dim_2').html(roomName + " TCP light brightness # %");
-				$('#command_dim_3').html(roomName + " brightness # %");
+				$('#command_dim_1').html(roomName + preface + " lights to # % brightness");
+				$('#command_dim_2').html("Dim " + roomName + preface + " lights to # %");
+				$('#command_dim_3').html("Set " + roomName + preface + " lights to # %");
 				$('#command_dim_response').html("Adjusting " + roomName + " lights");
 				$('#command_dim_url').html( externalAddr + ( (externalPort != 80 || externalPort != 443) ?  ':' + externalPort : '' ) + '/api.php?fx=dimby&type=room&uid=' + id + '&val={{NumberField}}' + ( requireExtPass == 1 ? '&password=' + externalPass : '') );
 				
-				$('#command_brighten_1').html("Brighten TCP " +  roomName + " lights by # %");
-				$('#command_brighten_2').html(roomName + "lights increase brightness by # %");
-				$('#command_brighten_3').html("Increase " + roomName + " brightness # %");
-				$('#command_brighten_response').html("Brightening " + roomName + " light");
+				$('#command_brighten_1').html("Increase " +  roomName +  preface + " lights brightness by # %");
+				$('#command_brighten_2').html("Brighten " + roomName + preface + " lights by # %");
+				$('#command_brighten_3').html("Up " + roomName + preface + " lights brightness # %");
+				$('#command_brighten_response').html("Brightening " + roomName + " lights");
 				$('#command_brighten_url').html( externalAddr + ( (externalPort != 80 || externalPort != 443) ?  ':' + externalPort : '' ) + '/api.php?fx=brightenby&type=room&uid=' + id + '&val={{NumberField}}' + ( requireExtPass == 1 ? '&password=' + externalPass : '') );
 				
-				$('#command_dimby_1').html("Dim TCP " +  roomName + " light by # %");
-				$('#command_dimby_2').html(roomName + " lights decrease brightness by # %");
-				$('#command_dimby_3').html("Decrease " + roomName + " brightness by # %");
+				$('#command_dimby_1').html("Decrease " +  roomName + preface + " lights brightness by # %");
+				$('#command_dimby_2').html("Darken " + roomName + preface +" lights by # %");
+				$('#command_dimby_3').html("Reduce " + roomName + preface + " lights brightness by # %");
 				$('#command_dimby_response').html("Dimming " + roomName + " lights");
 				$('#command_dimby_url').html( externalAddr + ( (externalPort != 80 || externalPort != 443) ?  ':' + externalPort : '' ) + '/api.php?fx=dimby&type=room&uid=' + id + '&val={{NumberField}}' + ( requireExtPass == 1 ? '&password=' + externalPass : '') );
 			}
 			
 			if( type == "light" ){
-				$('#command_on_1').html("TCP " +  roomName + " light ON");
-				$('#command_on_2').html(roomName + " TCP light ON");
-				$('#command_on_3').html(roomName + " light on");
+				roomName = $(this).find('option:selected').attr('data-device-name');
+				
+				$('#command_on_1').html( roomName + preface + " light ON" );
+				$('#command_on_2').html("Switch " + roomName + preface + " light ON");
+				$('#command_on_3').html("Turn " + roomName + preface + " light on");
 				$('#command_on_response').html("Turning " + roomName + " light ON");
 				$('#command_on_url').html( externalAddr + ( (externalPort != 80 || externalPort != 443) ?  ':' + externalPort : '' ) + '/api.php?fx=toggle&type=room&uid=' + id + '&val=1' + ( requireExtPass == 1 ? '&password=' + externalPass : '')  );
 				
-				$('#command_off_1').html("TCP " +  roomName + " light off");
-				$('#command_off_2').html(roomName + " TCP Light off");
-				$('#command_off_3').html(roomName + " light off");
+				$('#command_off_1').html( roomName + preface + " light off" );
+				$('#command_off_2').html("Switch " + roomName + preface + " light off");
+				$('#command_off_3').html("Turn " + roomName + preface + " light off");
 				$('#command_off_response').html("Turning off " + roomName + " light");
 				$('#command_off_url').html( externalAddr + ( (externalPort != 80 || externalPort != 443) ?  ':' + externalPort : '' ) + '/api.php?fx=toggle&type=room&uid=' + id + '&val=0' + ( requireExtPass == 1 ? '&password=' + externalPass : '') );
 				
-				$('#command_dim_1').html("TCP " +  roomName + " light to # % brightness");
-				$('#command_dim_2').html(roomName + " TCP light brightness # %");
-				$('#command_dim_3').html(roomName + " light brightness # %");
+				$('#command_dim_1').html( roomName +  preface + " light to # % brightness" );
+				$('#command_dim_2').html("Dim " + roomName + preface + " light to # %");
+				$('#command_dim_3').html("Set " + roomName + preface + " light brightness to # %");
 				$('#command_dim_response').html("Adjusting " + roomName + " brightness");
 				$('#command_dim_url').html( externalAddr + ( (externalPort != 80 || externalPort != 443) ?  ':' + externalPort : '' ) + '/api.php?fx=dimby&type=device&uid=' + id + '&val={{NumberField}}' + ( requireExtPass == 1 ? '&password=' + externalPass : '') );
 				
 				
-				$('#command_brighten_1').html("Brighten TCP" +  roomName + " light by # %");
-				$('#command_brighten_2').html(roomName + " light brightness increase by # %");
-				$('#command_brighten_3').html("increase " + roomName + " light brightness # %");
+				$('#command_brighten_1').html("Increase " +  roomName +  preface + " light brightness by # %");
+				$('#command_brighten_2').html("Brighten " + roomName + preface + " light by # %");
+				$('#command_brighten_3').html("Up " + roomName + preface + " light brightness to # %");
 				$('#command_brighten_response').html("Brightening " + roomName + " light");
 				$('#command_brighten_url').html( externalAddr + ( (externalPort != 80 || externalPort != 443) ?  ':' + externalPort : '' ) + '/api.php?fx=brightenby&type=device&uid=' + id + '&val={{NumberField}}' + ( requireExtPass == 1 ? '&password=' + externalPass : '') );
 				
-				$('#command_dimby_1').html("Dim TCP " +  roomName + " light by # %");
-				$('#command_dimby_2').html("Darken " + roomName + " light by # %");
-				$('#command_dimby_3').html("Decrease " + roomName + " brightness by # %");
-				$('#command_dimby_response').html("Dimming " + roomName + " lights");
+				$('#command_dimby_1').html("Decrease " +  roomName + preface + " light brightness by # %");
+				$('#command_dimby_2').html("Darken " + roomName + preface +" light by # %");
+				$('#command_dimby_3').html("Reduce " + roomName + preface + " light brightness by # %");
+				$('#command_dimby_response').html("Dimming " + roomName + " light");
 				$('#command_dimby_url').html( externalAddr + ( (externalPort != 80 || externalPort != 443) ?  ':' + externalPort : '' ) + '/api.php?fx=dimby&type=device&uid=' + id + '&val={{NumberField}}' + ( requireExtPass == 1 ? '&password=' + externalPass : '') );
 			}
 			
@@ -113,36 +122,49 @@ $(function(){
 			$('#sceneLights').show();
 			$('#roomLights').hide();
 			var sceneName = $(this).find('option:selected').attr('data-device-name');
-			console.log("Type: " + type + ", id: " + id + ", scene: " + sceneName );
+			//console.log("Type: " + type + ", id: " + id + ", scene: " + sceneName );
 			$('#appletImage').html('<img src="css/images/IFTTTAppletPhrase.png"/>');
 			
-			$('#scene_command_on_1').html("Activate " + sceneName );
-			$('#scene_command_on_2').html("Turn " + sceneName + " scene ON");
-			$('#scene_command_on_3').html(sceneName + " scene on");
-			$('#scene_command_on_response').html("Turning " + sceneName + " devices ON");
+			$('#scene_command_on_1').html("Activate " + sceneName + preface +" scene" );
+			$('#scene_command_on_2').html("Turn " + sceneName + preface +  " scene ON");
+			$('#scene_command_on_3').html(sceneName + preface + " scene on");
+			$('#scene_command_on_response').html("Turning " + sceneName + preface + " devices ON");
 			
 			$('#scene_command_on_url').html( externalAddr + ( externalPort != 80 ?  ':' + externalPort : '' ) + '/api.php?fx=scene&type=on&uid=' + id + '&val=1' + ( requireExtPass == 1 ? '&password=' + externalPass : '')  );
 			
-			$('#scene_command_off_1').html("Deactivate " +  sceneName);
-			$('#scene_command_off_2').html("Turn " + sceneName + " scene off");
-			$('#scene_command_off_3').html(sceneName + " off");
-			$('#scene_command_off_response').html("Turning " + sceneName + " devices off");
+			$('#scene_command_off_1').html("Deactivate " +  sceneName + preface + " scene");
+			$('#scene_command_off_2').html("Turn " + sceneName + preface + " scene off");
+			$('#scene_command_off_3').html(sceneName  + preface + " scene off");
+			$('#scene_command_off_response').html("Turning " + sceneName + preface + " devices off");
 			
 			$('#scene_command_off_url').html( externalAddr + ( externalPort != 80 ?  ':' + externalPort : '' ) + '/api.php?fx=scene&type=off&uid=' + id + '&val=0' + ( requireExtPass == 1 ? '&password=' + externalPass : '') );
 			
-			$('#command_run_1').html("Run " +  sceneName + " scene");
-			$('#command_run_2').html("Enable " + sceneName + " scene");
-			$('#command_run_3').html(sceneName + "scene on");
-			$('#command_run_response').html("Running " + sceneName + " scene");
+			$('#command_run_1').html("Activate " +  sceneName + preface +" scene");
+			$('#command_run_2').html("Turn " + sceneName + preface + " scene on");
+			$('#command_run_3').html(sceneName + " scene on");
+			$('#command_run_response').html("Running " + preface + sceneName + " scene");
 			
-			$('#command_run_url').html( externalAddr + ( externalPort != 80 ?  ':' + externalPort : '' ) + '/api.php?fx=scene&type=run&uid=' + id + '&val={{NumberField}}' + ( requireExtPass == 1 ? '&password=' + externalPass : '') );
+			$('#command_run_url').html( externalAddr + ( externalPort != 80 ?  ':' + externalPort : '' ) + '/api.php?fx=scene&type=run&uid=' + id + '&val=' + ( requireExtPass == 1 ? '&password=' + externalPass : '') );
 			
 		}
 
 	});
+	
+	$('.response').click(function(event){
+		event.preventDefault();
+		 var $temp = $("<input>");
+		$("body").append($temp);
+		$temp.val( $(this).text() ).select();
+		document.execCommand("copy");
+		$temp.remove();
+		console.log("Copying " + $(this).html() + " to Clipboard");
+
+	});
+	
 });
 </script>
-
+<p>Click on the <b>bolded</b> suggested responses to copy them to your clipboard</p>
+<p>Preface TCP Light commands with: <input type="text" id="preface" value="TCP" placeholder="TCP" /> (Can be helpful if you have multiple lighting systems with similar naming conventions.</p>
 <p>
 <select id="builder">
 	<option>Select a Device | Room | Scene</option>
@@ -165,11 +187,11 @@ $(function(){
 			}else{
 				$device = (array)$room["device"];
 				if( isset($device["did"]) ){
-					$DEVICES[] = "<option data-device-type='light' data-device-id='".$room["device"]["did"]."' data-room-name='".$room["name"]."'>".  $room["device"]["name"] ."</option>";
+					$DEVICES[] = "<option data-device-name='".$room["device"]["name"]."' data-device-type='light' data-device-id='".$room["device"]["did"]."' data-room-name='".$room["name"]."'>".  $room["device"]["name"] ."</option>";
 				}else{	
 					for( $x = 0; $x < sizeof($device); $x++ ){
 						if( isset($device[$x]) && is_array($device[$x]) && ! empty($device[$x]) ){
-							$DEVICES[] = '<option data-device-type="light" data-device-id="'.$device[$x]["did"].'" data-room-name="'.$room["name"].'">'. $device[$x]["name"] ."</option>";
+							$DEVICES[] = '<option  data-device-name="'.$device[$x]["name"].'" data-device-type="light" data-device-id="'.$device[$x]["did"].'" data-room-name="'.$room["name"].'">'. $device[$x]["name"] ."</option>";
 						}
 					}
 				}
