431b9c4d316c4c15081062c3773040e526ef3867
Paul <23568795+sktaylortrash@users.noreply.github.com>
2018-06-02 01:34:34 -0600

    Removed duplicate query for on/off Publish Status topic when state changes

--- mqtt_sub.py
+++ mqtt_sub.py
@@ -22,211 +22,175 @@ def on_message_control(client, userdata, msg):
 
 ### RearEntrance-BackDoor 
 def on_message_RearEntrance_BackDoor(client, userdata, msg):
-    if (msg.payload.decode() == '1'):
+    if (msg.payload.decode() == '0' or msg.payload.decode() == '1'):
         print ("RearEntrance BackDoor" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570733040739&val=1')
+        r = requests.get('https://lighting.taylortrash.com/api.php?fx=toggle&type=device&uid=216773570733040739&val=' + msg.payload.decode())
         r.json()
-    elif (msg.payload.decode() == '0'):
-        print ("RearEntrance BackDoor" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570733040739&val=0')
-        r.json()
+        client.publish("light/RearEntrance/BackDoor/216773570733040739/status", msg.payload.decode(), 0, True)
 
 def on_message_RearEntrance_BackDoor_Bright(client, userdata, msg):
     print ("RearEntrance BackDoor Brightness " + msg.payload.decode())
-    r = requests.get('https://lighting.lighting.local/api.php?fx=dim&type=device&uid=216773570733040739&val=' + msg.payload.decode())
+    r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570733040739&val=' + msg.payload.decode())
     r.json()
 
 
 ### Hallway-Hallway 
 def on_message_Hallway_Hallway(client, userdata, msg):
-    if (msg.payload.decode() == '1'):
+    if (msg.payload.decode() == '0' or msg.payload.decode() == '1'):
         print ("Hallway Hallway" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=360123189510580692&val=1')
+        r = requests.get('https://lighting.taylortrash.com/api.php?fx=toggle&type=device&uid=360123189510580692&val=' + msg.payload.decode())
         r.json()
-    elif (msg.payload.decode() == '0'):
-        print ("Hallway Hallway" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=360123189510580692&val=0')
-        r.json()
+        client.publish("light/Hallway/Hallway/360123189510580692/status", msg.payload.decode(), 0, True)
 
 def on_message_Hallway_Hallway_Bright(client, userdata, msg):
     print ("Hallway Hallway Brightness " + msg.payload.decode())
-    r = requests.get('https://lighting.lighting.local/api.php?fx=dim&type=device&uid=360123189510580692&val=' + msg.payload.decode())
+    r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=360123189510580692&val=' + msg.payload.decode())
     r.json()
 
 
 ### MasterBedroom-Ceiling1 
 def on_message_MasterBedroom_Ceiling1(client, userdata, msg):
-    if (msg.payload.decode() == '1'):
+    if (msg.payload.decode() == '0' or msg.payload.decode() == '1'):
         print ("MasterBedroom Ceiling1" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570733448185&val=1')
+        r = requests.get('https://lighting.taylortrash.com/api.php?fx=toggle&type=device&uid=216773570733448185&val=' + msg.payload.decode())
         r.json()
-    elif (msg.payload.decode() == '0'):
-        print ("MasterBedroom Ceiling1" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570733448185&val=0')
-        r.json()
+        client.publish("light/MasterBedroom/Ceiling1/216773570733448185/status", msg.payload.decode(), 0, True)
 
 def on_message_MasterBedroom_Ceiling1_Bright(client, userdata, msg):
     print ("MasterBedroom Ceiling1 Brightness " + msg.payload.decode())
-    r = requests.get('https://lighting.lighting.local/api.php?fx=dim&type=device&uid=216773570733448185&val=' + msg.payload.decode())
+    r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570733448185&val=' + msg.payload.decode())
     r.json()
 
 
 ### MasterBedroom-Ceiling2 
 def on_message_MasterBedroom_Ceiling2(client, userdata, msg):
-    if (msg.payload.decode() == '1'):
+    if (msg.payload.decode() == '0' or msg.payload.decode() == '1'):
         print ("MasterBedroom Ceiling2" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570733769850&val=1')
+        r = requests.get('https://lighting.taylortrash.com/api.php?fx=toggle&type=device&uid=216773570733769850&val=' + msg.payload.decode())
         r.json()
-    elif (msg.payload.decode() == '0'):
-        print ("MasterBedroom Ceiling2" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570733769850&val=0')
-        r.json()
+        client.publish("light/MasterBedroom/Ceiling2/216773570733769850/status", msg.payload.decode(), 0, True)
 
 def on_message_MasterBedroom_Ceiling2_Bright(client, userdata, msg):
     print ("MasterBedroom Ceiling2 Brightness " + msg.payload.decode())
-    r = requests.get('https://lighting.lighting.local/api.php?fx=dim&type=device&uid=216773570733769850&val=' + msg.payload.decode())
+    r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570733769850&val=' + msg.payload.decode())
     r.json()
 
 
 ### MasterBedroom-Lamp 
 def on_message_MasterBedroom_Lamp(client, userdata, msg):
-    if (msg.payload.decode() == '1'):
+    if (msg.payload.decode() == '0' or msg.payload.decode() == '1'):
         print ("MasterBedroom Lamp" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570733536747&val=1')
+        r = requests.get('https://lighting.taylortrash.com/api.php?fx=toggle&type=device&uid=216773570733536747&val=' + msg.payload.decode())
         r.json()
-    elif (msg.payload.decode() == '0'):
-        print ("MasterBedroom Lamp" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570733536747&val=0')
-        r.json()
+        client.publish("light/MasterBedroom/Lamp/216773570733536747/status", msg.payload.decode(), 0, True)
 
 def on_message_MasterBedroom_Lamp_Bright(client, userdata, msg):
     print ("MasterBedroom Lamp Brightness " + msg.payload.decode())
-    r = requests.get('https://lighting.lighting.local/api.php?fx=dim&type=device&uid=216773570733536747&val=' + msg.payload.decode())
+    r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570733536747&val=' + msg.payload.decode())
     r.json()
 
 
 ### Exterior-FrontDoor 
 def on_message_Exterior_FrontDoor(client, userdata, msg):
-    if (msg.payload.decode() == '1'):
+    if (msg.payload.decode() == '0' or msg.payload.decode() == '1'):
         print ("Exterior FrontDoor" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570734112971&val=1')
+        r = requests.get('https://lighting.taylortrash.com/api.php?fx=toggle&type=device&uid=216773570734112971&val=' + msg.payload.decode())
         r.json()
-    elif (msg.payload.decode() == '0'):
-        print ("Exterior FrontDoor" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570734112971&val=0')
-        r.json()
+        client.publish("light/Exterior/FrontDoor/216773570734112971/status", msg.payload.decode(), 0, True)
 
 def on_message_Exterior_FrontDoor_Bright(client, userdata, msg):
     print ("Exterior FrontDoor Brightness " + msg.payload.decode())
-    r = requests.get('https://lighting.lighting.local/api.php?fx=dim&type=device&uid=216773570734112971&val=' + msg.payload.decode())
+    r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570734112971&val=' + msg.payload.decode())
     r.json()
 
 
 ### Exterior-NWBackDoor 
 def on_message_Exterior_NWBackDoor(client, userdata, msg):
-    if (msg.payload.decode() == '1'):
+    if (msg.payload.decode() == '0' or msg.payload.decode() == '1'):
         print ("Exterior NWBackDoor" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570734219143&val=1')
+        r = requests.get('https://lighting.taylortrash.com/api.php?fx=toggle&type=device&uid=216773570734219143&val=' + msg.payload.decode())
         r.json()
-    elif (msg.payload.decode() == '0'):
-        print ("Exterior NWBackDoor" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570734219143&val=0')
-        r.json()
+        client.publish("light/Exterior/NWBackDoor/216773570734219143/status", msg.payload.decode(), 0, True)
 
 def on_message_Exterior_NWBackDoor_Bright(client, userdata, msg):
     print ("Exterior NWBackDoor Brightness " + msg.payload.decode())
-    r = requests.get('https://lighting.lighting.local/api.php?fx=dim&type=device&uid=216773570734219143&val=' + msg.payload.decode())
+    r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570734219143&val=' + msg.payload.decode())
     r.json()
 
 
 ### Exterior-NEDriveway 
 def on_message_Exterior_NEDriveway(client, userdata, msg):
-    if (msg.payload.decode() == '1'):
+    if (msg.payload.decode() == '0' or msg.payload.decode() == '1'):
         print ("Exterior NEDriveway" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570733998142&val=1')
+        r = requests.get('https://lighting.taylortrash.com/api.php?fx=toggle&type=device&uid=216773570733998142&val=' + msg.payload.decode())
         r.json()
-    elif (msg.payload.decode() == '0'):
-        print ("Exterior NEDriveway" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570733998142&val=0')
-        r.json()
+        client.publish("light/Exterior/NEDriveway/216773570733998142/status", msg.payload.decode(), 0, True)
 
 def on_message_Exterior_NEDriveway_Bright(client, userdata, msg):
     print ("Exterior NEDriveway Brightness " + msg.payload.decode())
-    r = requests.get('https://lighting.lighting.local/api.php?fx=dim&type=device&uid=216773570733998142&val=' + msg.payload.decode())
+    r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570733998142&val=' + msg.payload.decode())
     r.json()
 
 
 ### Exterior-SEDriveway 
 def on_message_Exterior_SEDriveway(client, userdata, msg):
-    if (msg.payload.decode() == '1'):
+    if (msg.payload.decode() == '0' or msg.payload.decode() == '1'):
         print ("Exterior SEDriveway" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570733525707&val=1')
+        r = requests.get('https://lighting.taylortrash.com/api.php?fx=toggle&type=device&uid=216773570733525707&val=' + msg.payload.decode())
         r.json()
-    elif (msg.payload.decode() == '0'):
-        print ("Exterior SEDriveway" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570733525707&val=0')
-        r.json()
+        client.publish("light/Exterior/SEDriveway/216773570733525707/status", msg.payload.decode(), 0, True)
 
 def on_message_Exterior_SEDriveway_Bright(client, userdata, msg):
     print ("Exterior SEDriveway Brightness " + msg.payload.decode())
-    r = requests.get('https://lighting.lighting.local/api.php?fx=dim&type=device&uid=216773570733525707&val=' + msg.payload.decode())
+    r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570733525707&val=' + msg.payload.decode())
     r.json()
 
 
 ### LivingRoom-Fan 
 def on_message_LivingRoom_Fan(client, userdata, msg):
-    if (msg.payload.decode() == '1'):
+    if (msg.payload.decode() == '0' or msg.payload.decode() == '1'):
         print ("LivingRoom Fan" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=359841714533870036&val=1')
+        r = requests.get('https://lighting.taylortrash.com/api.php?fx=toggle&type=device&uid=359841714533870036&val=' + msg.payload.decode())
         r.json()
-    elif (msg.payload.decode() == '0'):
-        print ("LivingRoom Fan" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=359841714533870036&val=0')
-        r.json()
+        client.publish("light/LivingRoom/Fan/359841714533870036/status", msg.payload.decode(), 0, True)
 
 def on_message_LivingRoom_Fan_Bright(client, userdata, msg):
     print ("LivingRoom Fan Brightness " + msg.payload.decode())
-    r = requests.get('https://lighting.lighting.local/api.php?fx=dim&type=device&uid=359841714533870036&val=' + msg.payload.decode())
+    r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=359841714533870036&val=' + msg.payload.decode())
     r.json()
 
 
 ### Bathroom-LED 
 def on_message_Bathroom_LED(client, userdata, msg):
-    if (msg.payload.decode() == '1'):
+    if (msg.payload.decode() == '0' or msg.payload.decode() == '1'):
         print ("Bathroom LED" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570733505864&val=1')
+        r = requests.get('https://lighting.taylortrash.com/api.php?fx=toggle&type=device&uid=216773570733505864&val=' + msg.payload.decode())
         r.json()
-    elif (msg.payload.decode() == '0'):
-        print ("Bathroom LED" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570733505864&val=0')
-        r.json()
+        client.publish("light/Bathroom/LED/216773570733505864/status", msg.payload.decode(), 0, True)
 
 def on_message_Bathroom_LED_Bright(client, userdata, msg):
     print ("Bathroom LED Brightness " + msg.payload.decode())
-    r = requests.get('https://lighting.lighting.local/api.php?fx=dim&type=device&uid=216773570733505864&val=' + msg.payload.decode())
+    r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570733505864&val=' + msg.payload.decode())
     r.json()
 
 
 ### FutureUse-LED 
 def on_message_FutureUse_LED(client, userdata, msg):
-    if (msg.payload.decode() == '1'):
+    if (msg.payload.decode() == '0' or msg.payload.decode() == '1'):
         print ("FutureUse LED" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570734215492&val=1')
+        r = requests.get('https://lighting.taylortrash.com/api.php?fx=toggle&type=device&uid=216773570734215492&val=' + msg.payload.decode())
         r.json()
-    elif (msg.payload.decode() == '0'):
-        print ("FutureUse LED" + msg.payload.decode())
-        r = requests.get('https://lighting.lighting.local/api.php?fx=toggle&type=device&uid=216773570734215492&val=0')
-        r.json()
+        client.publish("light/FutureUse/LED/216773570734215492/status", msg.payload.decode(), 0, True)
 
 def on_message_FutureUse_LED_Bright(client, userdata, msg):
     print ("FutureUse LED Brightness " + msg.payload.decode())
-    r = requests.get('https://lighting.lighting.local/api.php?fx=dim&type=device&uid=216773570734215492&val=' + msg.payload.decode())
+    r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570734215492&val=' + msg.payload.decode())
     r.json()
 
 
 
 client = mqtt.Client('tcp-subscriber')               #create new instance
-client.username_pw_set('admin', password='Password')    #set username and password
+client.username_pw_set('admin', password='zhgcfn')    #set username and password
 
 #Callbacks that trigger on a specific subscription match
 client.message_callback_add('control', on_message_control)
