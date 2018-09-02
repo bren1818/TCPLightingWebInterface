a5ed7a03391121db941b6aa47c914a4eab5fc534
Paul <23568795+sktaylortrash@users.noreply.github.com>
2018-09-02 00:05:17 -0600

    updates

--- mqtt_sub.py
+++ mqtt_sub.py
@@ -33,6 +33,7 @@ def on_message_RearEntrance_BackDoor_Bright(client, userdata, msg):
     r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570733040739&val=' + msg.payload.decode())
     r.json()
 
+    client.publish("light/RearEntrance/BackDoor/216773570733040739/brightness", msg.payload.decode(), 0, True)
 
 ### Hallway-Hallway 
 def on_message_Hallway_Hallway(client, userdata, msg):
@@ -47,6 +48,7 @@ def on_message_Hallway_Hallway_Bright(client, userdata, msg):
     r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=360123189510580692&val=' + msg.payload.decode())
     r.json()
 
+    client.publish("light/Hallway/Hallway/360123189510580692/brightness", msg.payload.decode(), 0, True)
 
 ### MasterBedroom-Ceiling1 
 def on_message_MasterBedroom_Ceiling1(client, userdata, msg):
@@ -61,6 +63,7 @@ def on_message_MasterBedroom_Ceiling1_Bright(client, userdata, msg):
     r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570733448185&val=' + msg.payload.decode())
     r.json()
 
+    client.publish("light/MasterBedroom/Ceiling1/216773570733448185/brightness", msg.payload.decode(), 0, True)
 
 ### MasterBedroom-Ceiling2 
 def on_message_MasterBedroom_Ceiling2(client, userdata, msg):
@@ -75,6 +78,7 @@ def on_message_MasterBedroom_Ceiling2_Bright(client, userdata, msg):
     r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570733769850&val=' + msg.payload.decode())
     r.json()
 
+    client.publish("light/MasterBedroom/Ceiling2/216773570733769850/brightness", msg.payload.decode(), 0, True)
 
 ### MasterBedroom-Lamp 
 def on_message_MasterBedroom_Lamp(client, userdata, msg):
@@ -89,6 +93,7 @@ def on_message_MasterBedroom_Lamp_Bright(client, userdata, msg):
     r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570733536747&val=' + msg.payload.decode())
     r.json()
 
+    client.publish("light/MasterBedroom/Lamp/216773570733536747/brightness", msg.payload.decode(), 0, True)
 
 ### Exterior-FrontDoor 
 def on_message_Exterior_FrontDoor(client, userdata, msg):
@@ -103,6 +108,7 @@ def on_message_Exterior_FrontDoor_Bright(client, userdata, msg):
     r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570734112971&val=' + msg.payload.decode())
     r.json()
 
+    client.publish("light/Exterior/FrontDoor/216773570734112971/brightness", msg.payload.decode(), 0, True)
 
 ### Exterior-NWBackDoor 
 def on_message_Exterior_NWBackDoor(client, userdata, msg):
@@ -117,6 +123,7 @@ def on_message_Exterior_NWBackDoor_Bright(client, userdata, msg):
     r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570734219143&val=' + msg.payload.decode())
     r.json()
 
+    client.publish("light/Exterior/NWBackDoor/216773570734219143/brightness", msg.payload.decode(), 0, True)
 
 ### Exterior-NEDriveway 
 def on_message_Exterior_NEDriveway(client, userdata, msg):
@@ -131,6 +138,7 @@ def on_message_Exterior_NEDriveway_Bright(client, userdata, msg):
     r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570733998142&val=' + msg.payload.decode())
     r.json()
 
+    client.publish("light/Exterior/NEDriveway/216773570733998142/brightness", msg.payload.decode(), 0, True)
 
 ### Exterior-SEDriveway 
 def on_message_Exterior_SEDriveway(client, userdata, msg):
@@ -145,6 +153,7 @@ def on_message_Exterior_SEDriveway_Bright(client, userdata, msg):
     r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570733525707&val=' + msg.payload.decode())
     r.json()
 
+    client.publish("light/Exterior/SEDriveway/216773570733525707/brightness", msg.payload.decode(), 0, True)
 
 ### LivingRoom-Fan 
 def on_message_LivingRoom_Fan(client, userdata, msg):
@@ -159,6 +168,7 @@ def on_message_LivingRoom_Fan_Bright(client, userdata, msg):
     r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=359841714533870036&val=' + msg.payload.decode())
     r.json()
 
+    client.publish("light/LivingRoom/Fan/359841714533870036/brightness", msg.payload.decode(), 0, True)
 
 ### Bathroom-LED 
 def on_message_Bathroom_LED(client, userdata, msg):
@@ -173,6 +183,7 @@ def on_message_Bathroom_LED_Bright(client, userdata, msg):
     r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570733505864&val=' + msg.payload.decode())
     r.json()
 
+    client.publish("light/Bathroom/LED/216773570733505864/brightness", msg.payload.decode(), 0, True)
 
 ### FutureUse-LED 
 def on_message_FutureUse_LED(client, userdata, msg):
@@ -187,6 +198,7 @@ def on_message_FutureUse_LED_Bright(client, userdata, msg):
     r = requests.get('https://lighting.taylortrash.com/api.php?fx=dim&type=device&uid=216773570734215492&val=' + msg.payload.decode())
     r.json()
 
+    client.publish("light/FutureUse/LED/216773570734215492/brightness", msg.payload.decode(), 0, True)
 
 
 client = mqtt.Client('tcp-subscriber')               #create new instance
