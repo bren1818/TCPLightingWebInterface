89a6e5cdd7d4c8e69a8bd3ba728c8bfcfa1d7dbe
Paul <23568795+sktaylortrash@users.noreply.github.com>
2019-09-12 21:37:27 -0600

    Update mqttstate.php
    Added delay for slower broker response

--- mqttstate.php
+++ mqttstate.php
@@ -92,6 +92,7 @@ require("phpMQTT/phpMQTT.php");
 								$mqtt->publish($MQTT_prefix.'/'.$RoomName.'/'.$DeviceName.'/'.$device['did'].'/status', $device['state']);
 								$mqtt->publish($MQTT_prefix.'/'.$RoomName.'/'.$DeviceName.'/'.$device['did'].'/brightness', $device['level']);
 								$mqtt->publish($MQTT_prefix.'/'.$RoomName.'/'.$DeviceName.'/'.$device['did'].'/LWT', $LWT);
+								sleep(1);
 								echo $DeviceName.': On-Off State: '.$device["state"].'  Brightness:'.$device["level"].'  Online Status '.$LWT.'<br/>';
 							}
 							$mqtt->close();
